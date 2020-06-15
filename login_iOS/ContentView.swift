d281b8a18387e7a2f178f1d7767d67de1d30fdd9
Tsungyu Yu <ytyubox@gmail.com>
2020-06-15 20:15:26 +0800

    Initial Commit

---
+++ login_iOS/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  login_iOS
+//
+//  Created by 游宗諭 on 2020/6/15.
+//  Copyright © 2020 ytyubox. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
